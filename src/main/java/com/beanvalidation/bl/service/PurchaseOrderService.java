51d27ac4dc56e7a7942ae02875cd327e95ee241f
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-01-01 13:26:54 -0500

    Added Service which demonstrates how to construct a Validator Using ValidatorFactory and to throw a ConstraintViolation if validate fails for Customer

--- src/main/java/com/beanvalidation/bl/service/PurchaseOrderService.java
+++ src/main/java/com/beanvalidation/bl/service/PurchaseOrderService.java
@@ -1,4 +1,37 @@
 package com.beanvalidation.bl.service;
 
+import com.beanvalidation.data.Customer;
+
+import javax.validation.*;
+import java.util.Set;
+
 public class PurchaseOrderService {
+
+    /*
+     * Construct a validator using Validator factory
+     */
+
+    private ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+
+    private Validator validator = validatorFactory.getValidator();
+
+    private Set<ConstraintViolation<Customer>> violations;
+
+    public void createCustomer(Customer customer){
+        violations = validator.validate(customer);
+        if(violations.size() > 0){
+            throw new ConstraintViolationException(violations);
+        }
+        //If no violations are there , Create a Customer
+    }
+
+    public void updateCustomer(Customer customer){
+        violations = validator.validate(customer);
+        if(!violations.isEmpty()){
+            throw new ConstraintViolationException(violations);
+        }
+        //If no exceptions are there, Update a Customer
+    }
+
+
 }
--- src/test/java/com/beanvalidation/bl/service/PurchaseOrderServiceSpec.groovy
+++ src/test/java/com/beanvalidation/bl/service/PurchaseOrderServiceSpec.groovy
@@ -1,7 +1,47 @@
 package com.beanvalidation.bl.service
 
+import com.beanvalidation.data.Customer
 import spock.lang.Specification
 
-class PurchaseOrderServiceSpec extends Specification{
+import javax.validation.ConstraintViolation
+import javax.validation.ConstraintViolationException
+import javax.validation.Validator
+import javax.validation.ValidatorFactory
 
+class PurchaseOrderServiceSpec extends Specification{
+    ValidatorFactory validatorFactory = Mock()
+    Validator validator = Mock()
+
+    PurchaseOrderService bl = new PurchaseOrderService(validatorFactory: validatorFactory,
+                                                        validator: validator)
+
+    def "test - createCustomer - valid"(){
+        given:
+        Customer customer = new Customer('Gagan', 'Bhatnagar', 'gagan@gmail.com', '149-89607867', '10-10-1982');
+
+        when:
+        bl.createCustomer(customer)
+
+        then:
+        1 * validator.validate(customer) >> []
+        0 * _
+
+        and:
+        noExceptionThrown()
+    }
+
+    def "test - updateCustomer - valid"(){
+        given:
+        Customer customer = new Customer('Amit', 'Bhatnagar', 'gagan@gmail.com', '149-89607867', '10-10-1982');
+
+        when:
+        bl.createCustomer(customer)
+
+        then:
+        1 * validator.validate(customer) >> []
+        0 * _
+
+        and:
+        noExceptionThrown()
+    }
 }
